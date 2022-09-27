e8ffe541b26386485cb711783e83a2d2b2157d3f
Rajesh Suresh Gaikwad <rajesh.gaikwad@yash.com>
2022-09-27 18:06:59 +0530

    authentication app with firebase

---
+++ Authentication app with firebase/src/components/Layout/MainNavigation.js
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import classes from './MainNavigation.module.css';
+import AuthContext from '../../Store/Auth-context';
+
+const MainNavigation = () => {
+  const authCtx= useContext(AuthContext);
+  const isloggedIn =authCtx.isLoggedIn;
+
+  const logoutHandler =()=>{
+    authCtx.logout();
+  }
+  return (
+    <header className={classes.header}>
+      <Link to='/'>
+        <div className={classes.logo}>React Auth</div>
+      </Link>
+      <nav>
+        <ul>
+        {!isloggedIn && <li>
+            <Link to='/auth'>Login</Link>
+          </li> }
+          
+          {isloggedIn && <li>
+            <Link to='/profile'>Profile</Link>
+          </li>
+          }
+          
+          {isloggedIn && <li>
+            <button onClick={logoutHandler}>Logout</button>
+          </li>
+          }
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
---
+++ Authentication app with firebase/src/components/Profile/ProfileForm.js
@@ -0,0 +1,42 @@
+import classes from "./ProfileForm.module.css";
+import { useRef, useContext } from "react";
+import AuthContext from "../../Store/Auth-context";
+const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAOeoSld0gPx7e50W1N1k8MawxPav7-Phc",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: enteredNewPassword,
+          returnSecureToken: false,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => {
+      // assumption always success
+    });
+  };
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <div className={classes.control}>
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" ref={newPasswordInputRef} />
+      </div>
+      <div className={classes.action}>
+        <button>Change Password</button>
+      </div>
+    </form>
+  );
+};
+
+export default ProfileForm;
