7119559ca61da383ba5cd7b751b19d149de6cbee
Rajesh Suresh Gaikwad <rajesh.gaikwad@yash.com>
2022-10-10 19:28:42 +0530

    chating app

---
+++ Chat app/src/App.js
@@ -0,0 +1,15 @@
+import { ChatEngine } from 'react-chat-engine';
+import './App.css';
+
+const App=()=> {
+  return (
+    <ChatEngine 
+       height="100vh"
+      projectID="13209285-6377-4e71-91a1-c9e477456643"
+      userName="Rajesh"
+      userSecret="123"
+    />
+  );
+}
+
+export default App;
